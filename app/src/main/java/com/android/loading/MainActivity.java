6980916a3f7d91625ab10658b7bd4e43558887c0
琦琦 <329191055@qq.com>
2017-02-12 23:05:09 +0800

    数据  控件 绑定

--- app/src/main/java/com/android/loading/MainActivity.java
+++ app/src/main/java/com/android/loading/MainActivity.java
@@ -2,19 +2,50 @@ package com.android.loading;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.loading.widget.LoadingLayout;
 
-import butterknife.BindView;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.ll_lading)
-    LoadingLayout ll_lading;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private LoadingLayout ll_lading;
+    private Button bt_no_network, bt_erro, bt_empty, bt_customer, bt_reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ll_lading = (LoadingLayout) findViewById(R.id.ll_lading);
+        bt_no_network = (Button) findViewById(R.id.bt_no_network);
+        bt_erro = (Button) findViewById(R.id.bt_erro);
+        bt_empty = (Button) findViewById(R.id.bt_empty);
+        bt_customer = (Button) findViewById(R.id.bt_customer);
+        bt_reset = (Button) findViewById(R.id.bt_reset);
+        bt_no_network.setOnClickListener(this);
+        bt_erro.setOnClickListener(this);
+        bt_empty.setOnClickListener(this);
+        bt_customer.setOnClickListener(this);
+        bt_reset.setOnClickListener(this);
         ll_lading.setStatus(LoadingLayout.Loading);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_no_network:
+                ll_lading.setStatus(LoadingLayout.No_Network);
+                break;
+            case R.id.bt_erro:
+                ll_lading.setStatus(LoadingLayout.Error);
+                break;
+            case R.id.bt_empty:
+                ll_lading.setStatus(LoadingLayout.Empty);
+                break;
+            case R.id.bt_customer:
+                break;
+            case R.id.bt_reset:
+                break;
+        }
     }
 }
