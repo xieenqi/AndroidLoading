0ac7ecf20f247ba59b01af9b8db16715b3240242
琦琦 <329191055@qq.com>
2017-02-12 22:28:49 +0800

    loading view 自定义

--- app/src/main/java/com/android/loading/MainActivity.java
+++ app/src/main/java/com/android/loading/MainActivity.java
@@ -3,11 +3,18 @@ package com.android.loading;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.loading.widget.LoadingLayout;
+
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.ll_lading)
+    LoadingLayout ll_lading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ll_lading.setStatus(LoadingLayout.Loading);
     }
 }
